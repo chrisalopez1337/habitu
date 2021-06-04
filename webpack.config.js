5477a709e4ffb6daf02be78ebf286f453b3ddfbe
sakura <sakura@sakuras-MacBook-Air.local>
2021-06-03 17:04:18 -0700

    Init base file structure

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const sourceDir = path.join(__dirname, '/client/src');
+const distDir = path.join(__dirname, '/client/dist');
+
+module.exports = {
+    entry: `${sourceDir}/index.jsx`,
+    output: {
+        filename: 'bundle.js',
+        path: distDir,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
